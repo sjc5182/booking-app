d015b18949188eb32520392a30568167ec2eebde
sjc5182 <shiguangchen1987@gmail.com>
2019-01-10 23:24:48 -0800

    logic for search

--- src/components/ItemListDrop.js
+++ src/components/ItemListDrop.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import '../style/booking.css';
 
-// export default class ItemListDrop extends React.Component{
-//   render(){
-//     const { test } = this.props
-//     return(
-//       <div>{console.log(test)}</div>
-//     )
-//   }
-// };
+export default class ItemListDrop extends React.Component{
+  state = {
+    targetList: null
+  }
+  
+  itemClick = (e) => {
+    this.setState({
+      targetList: e.target.value
+    })
+  }
+  
+  render(){
+    const { test } = this.props
+    return(
+      <div>
+      <div className="itemlist">
+        {test.dropdownlist.map((item) => (
+          <li key={item.id} onClick={this.itemClick} value= {item.id}>{item.name}</li>
+        ))}
+      </div>
+      <button onClick= {this.itemClick}>hello</button>
+    </div>
+    )
+  }
+};
 
-const ItemListDrop = ({test}) => (
-  // <div className="outer-wrapper">
-  //   <div className="">
-      
-  //   </div>
-  // </div>
-
-  //<div>{console.log(test)}</div>
-  <div className="itemlist">
-    {test.dropdownlist.map((item) => (
-        //console.log(item.name)
-        <li key={item.id}>{item.name}</li>
-    ))}
-  </div>
-);
-
-export default ItemListDrop;
